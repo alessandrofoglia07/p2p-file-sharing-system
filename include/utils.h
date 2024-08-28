6b79b2c6e40baa70907e3cb384c8da24707fabb9
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-08-28 11:21:44 +0200

    Add should_transfer_file, serialize_file_entries, deserialize_file_entries, delete_transferred_files functions to utils.c/h

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <file_entry.h>
 #include <stdint.h>
 
 // checks if id is in the interval (a, b)
@@ -8,5 +9,15 @@ int is_in_interval(const uint8_t *id, const uint8_t *a, const uint8_t *b);
 
 // generate unique 32 bits id
 uint32_t generate_id();
+
+// checks if file_id falls in the interval (new_node_id, current_node_id]
+int should_transfer_file(const uint8_t *file_id, const uint8_t *new_node_id, const uint8_t *current_node_id);
+
+size_t serialize_file_entries(char *buf, size_t buf_size, FileEntry *files, const uint8_t *new_node_id,
+                              const uint8_t *current_node_id);
+
+void deserialize_file_entries(Node *n, const char *buf, size_t buf_size);
+
+void delete_transferred_files(FileEntry **pFiles, const uint8_t *new_node_id, const uint8_t *current_node_id);
 
 #endif //UTILS_H
--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,8 @@
 #include "utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "sha1.h"
 #include <string.h>
 #include <arpa/inet.h>
@@ -17,4 +21,74 @@ uint32_t generate_id() {
     uuid_t uuid;
     uuid_generate(uuid);
     return *(uint32_t *) uuid; // return the first 32 bits
+}
+
+// checks if file_id falls in the interval (new_node_id, current_node_id]
+int should_transfer_file(const uint8_t *file_id, const uint8_t *new_node_id, const uint8_t *current_node_id) {
+    return memcmp(file_id, new_node_id, HASH_SIZE) > 0 && memcmp(file_id, current_node_id, HASH_SIZE) <= 0;
+}
+
+size_t serialize_file_entries(char *buf, size_t buf_size, FileEntry *files, const uint8_t *new_node_id,
+                              const uint8_t *current_node_id) {
+    size_t offset = 0;
+    const FileEntry *entry = files;
+
+    while (entry != NULL && offset < buf_size) {
+        if (should_transfer_file(entry->id, new_node_id, current_node_id)) {
+            const size_t entry_size = sizeof(FileEntry);
+            if (offset + entry_size > buf_size) {
+                break; // not enough space
+            }
+
+            memcpy(buf + offset, entry, entry_size);
+            offset += entry_size;
+        }
+        entry = entry->next;
+    }
+
+    return offset;
+}
+
+void deserialize_file_entries(Node *n, const char *buf, size_t buf_size) {
+    size_t offset = 0;
+
+    while (offset < buf_size) {
+        // allocate memory for new file entry
+        FileEntry *entry = (FileEntry *) malloc(sizeof(FileEntry));
+        if (entry == NULL) {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+
+        memcpy(entry, buf + offset, sizeof(FileEntry));
+        entry->next = NULL; // make sure the next pointer is NULL
+
+        entry->next = n->files;
+        n->files = entry;
+
+        offset += sizeof(FileEntry);
+    }
+}
+
+void delete_transferred_files(FileEntry **pFiles, const uint8_t *new_node_id, const uint8_t *current_node_id) {
+    FileEntry *prev = NULL;
+    FileEntry *entry = *pFiles;
+
+    while (entry != NULL) {
+        if (should_transfer_file(entry->id, new_node_id, current_node_id)) {
+            if (prev == NULL) {
+                // removing the head of the file
+                *pFiles = entry->next;
+            } else {
+                prev->next = entry->next;
+            }
+
+            FileEntry *to_delete = entry;
+            entry = entry->next;
+            free(to_delete); // free the memory
+        } else {
+            prev = entry;
+            entry = entry->next;
+        }
+    }
 }
